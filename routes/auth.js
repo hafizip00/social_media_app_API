fd683b7ae6a5810d46bec494564d95c951e0b27c
hafizip00 <hafizip00@gmail.com>
2022-06-20 01:25:30 +0500

    Register And Login API

--- routes/auth.js
+++ routes/auth.js
@@ -1,16 +1,34 @@
-const route = require('express').Router()
-const User = require('../models/User')
+const route = require("express").Router();
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
 
-route.get("/register" , async (req , res) =>{
+route.post("/register", async (req, res) => {
+  try {
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
     const user = new User({
-        username : "John" ,
-        email : "johndoe@gmail.com",
-        password : "123456"
-    })
+      username: req.body.username,
+      email: req.body.email,
+      password: hashedPassword,
+    });
+    await user.save();
+    res.status(200).send(user);
+  } catch (error) {
+    res.send("error");
+  }
+});
 
-    await user.save()
-    res.send("USER ADDEDDDD")
+route.post("/login" , async (req , res)=>{
+    try {
+        const user = await User.findOne({email : req.body.email})
+    !user && res.status(404).send("Account Not Found Please Register First!")
+    const validPassword  = await bcrypt.compare(req.body.password , user.password)
+    !validPassword && res.status(400).json("Wrong Password!")
+
+    res.status(200).json(user)
+    } catch (error) {
+        console.log(error);
+    }
 })
-
 
 module.exports = route;
